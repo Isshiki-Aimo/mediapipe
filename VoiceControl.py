7cee4baa3e82edf017f16083c98875eb0cdaef52
Aimo <askuasweet@gmail.com>
2022-08-21 19:35:43 +0800

    修改了进入退出功能，但仍需完善

--- VoiceControl.py
+++ VoiceControl.py
@@ -8,6 +8,7 @@ from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
 import HandTrackingModule as htm
+from util import compute_distance
 
 devices = AudioUtilities.GetSpeakers()
 interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
@@ -25,45 +26,85 @@ cap.set(4, hCam)
 pTime = 0
 detector = htm.handDetector()
 
-no_act_thres = 15  # 可以容忍的错误帧数
-stop_thres = 40  # 判断为停滞的移动距离
-stable_thres = 10  # 判断为稳定触发的时间
+stop_thres = 15  # 判断为停滞的移动距离
+stable_thres = 20  # 判断为稳定触发的时间
+stop_time = [0]
+stop_time1 = [-50]
+old_lmList = None
+functionflag = False
+draw = True
 
 while True:
     ret, img = cap.read()
     img = detector.findHands(img)
     lmList = detector.findPosition(img)
 
-    if len(lmList) != 0:
+    if old_lmList is None and len(lmList):
         old_lmList = lmList
-        x1, y1, x2, y2 = lmList[4][1], lmList[4][2], lmList[8][1], lmList[8][2]
-        xc, yc = (x2 + x1) // 2, (y2 + y1) // 2
-        cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
-        cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
-        cv2.circle(img, (xc, yc), 15, (255, 0, 255), cv2.FILLED)
-        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
-        length = math.hypot(x2 - x1, y2 - y1)  # 15--200
-        print(length)
-        if length < 25:
-            cv2.circle(img, (xc, yc), 15, (0, 255, 0), cv2.FILLED)
-
+    if len(lmList):
         # 下面实现长度到音量的转换
-        # np.interp为插值函数，简而言之，看line_len的值在[15，200]中所占比例，然后去[min_volume,max_volume]中线性寻找相应的值，作为返回值
-        vol = np.interp(length, [15, 200], [minVol, maxVol])
-        # 用之前得到的vol值设置电脑音量
-        volume.SetMasterVolumeLevel(vol, None)
-        volBar = np.interp(length, [15, 200], [350, 150])
-        volPer = np.interp(length, [15, 200], [0, 100])
+        # 判断食指是否稳定
+        stop_length = compute_distance(lmList[8][1], lmList[8][2], old_lmList[8][1], old_lmList[8][2])
+        fingers = detector.fingersUp()
 
-        cv2.rectangle(img, (20, 150), (50, 350), (255, 0, 255), 2)
-        cv2.rectangle(img, (20, int(volBar)), (50, 350), (255, 0, 255), cv2.FILLED)
-        cv2.putText(img, f'{int(volPer)}%', (10, 380), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
+        if fingers[0] and fingers[1]:
+            if stop_length < stop_thres or functionflag:
+                if stop_time[0] < stable_thres:
+                    stop_time[0] += 1
+                fill_cnt = stop_time[0] / stable_thres * 360
+                cv2.circle(img, (lmList[8][1], lmList[8][2]), 15, (0, 255, 0), cv2.FILLED)
+                if 0 < fill_cnt < 360:
+                    cv2.ellipse(img, (lmList[8][1], lmList[8][2]), (30, 30), 0, 0, fill_cnt, (255, 255, 0),
+                                2)
+                    # 进入功能调节音量
+                else:
+                    if draw:
+                        cv2.ellipse(img, (lmList[8][1], lmList[8][2]), (30, 30), 0, 0, fill_cnt, (0, 150, 255),
+                                    4)
+                        functionflag = True
+                        old_lmList = lmList
+                        x1, y1, x2, y2 = lmList[4][1], lmList[4][2], lmList[8][1], lmList[8][2]
+                        xc, yc = (x2 + x1) // 2, (y2 + y1) // 2
+                        cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+                        cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+                        cv2.circle(img, (xc, yc), 15, (255, 0, 255), cv2.FILLED)
+                        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
+                        length = math.hypot(x2 - x1, y2 - y1)  # 15--200
+                        if length < 25:
+                            cv2.circle(img, (xc, yc), 15, (0, 255, 0), cv2.FILLED)
 
+                        # 下面实现长度到音量的转换
+                        # np.interp为插值函数，简而言之，看line_len的值在[15，200]中所占比例，然后去[min_volume,max_volume]中线性寻找相应的值，作为返回值
+                        vol = np.interp(length, [15, 200], [minVol, maxVol])
+                        # 用之前得到的vol值设置电脑音量
+                        volume.SetMasterVolumeLevel(vol, None)
+                        volBar = np.interp(length, [15, 200], [350, 150])
+                        volPer = np.interp(length, [15, 200], [0, 100])
 
+                        cv2.rectangle(img, (20, 150), (50, 350), (255, 0, 255), 2)
+                        cv2.rectangle(img, (20, int(volBar)), (50, 350), (255, 0, 255), cv2.FILLED)
+                        cv2.putText(img, f'{int(volPer)}%', (10, 380), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
 
+                    if stop_length < stop_thres + 15:
+                        if stop_time1[0] < stable_thres:
+                            stop_time1[0] += 1
+                        fill_cnt = stop_time1[0] / stable_thres * 360
+                        cv2.circle(img, (lmList[8][1], lmList[8][2]), 15, (0, 255, 0), cv2.FILLED)
+                        if 0 < fill_cnt < 360:
+                            cv2.ellipse(img, (lmList[8][1], lmList[8][2]), (30, 30), 0, 0, fill_cnt, (255, 255, 0),
+                                        2)
+                        if fill_cnt == 0:
+                            draw = False
 
+            else:
+                stop_time[0] = 0
+
+        else:
+            stop_time[0] = 0
+
+        old_lmList = lmList
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
