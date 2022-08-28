50876f75d71448841d5e7212481bbd962e27c1b0
Aimo <askuasweet@gmail.com>
2022-08-28 16:53:41 +0800

    视频控制2.0

--- util.py
+++ util.py
@@ -4,7 +4,6 @@ import math
 
 def compute_distance(x1, y1, x2, y2):
     return math.hypot(x1 - x2, y1 - y2)
-    # return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
 
 def compute_direction(x1, y1, x2, y2):
